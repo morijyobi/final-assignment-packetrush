3ef4fd324346a090a9210e7e7e03737c5f1c1cb8
sugata <k.sugata.sys24@morijyobi.ac.jp>
2025-06-23 09:36:13 +0900

    ディレクトリ構成の構築

---
+++ client/game.py
@@ -0,0 +1,8 @@
+#ソケットの通信の準備、プレイヤー情報の送受信など
+import pygame
+import socket
+import json
+from player import Player
+from utils import config
+class Game:
+    pass
---
+++ client/player.py
@@ -0,0 +1,6 @@
+#プレイヤーの動きと表示を管理
+import pygame
+import uuid
+from utils import config
+class Player:
+    pass
