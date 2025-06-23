288fb6319904cfa40f32a9db3f3f1d14c67a3b66
kyo <k.nozaki.sys24@morijyobi.ac.jp>
2025-06-23 10:54:00 +0900

    簡単なソケットの作成

--- server/server.py
+++ server/server.py
@@ -2,3 +2,14 @@
 import socket
 import threading
 import json
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_addr = ('localhost', 12345)
+
+while True:
+    msg = input("送信するメッセージ > ")
+    if msg == "exit":
+        break
+    client_socket.sendto(msg.encode(), server_addr)
+    data, _ = client_socket.recvfrom(1024)
+    print("[サーバーの応答]:", data.decode())
