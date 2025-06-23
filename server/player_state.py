cce6db32213026de961b6b0a8d27686792dfc446
sugata <k.sugata.sys24@morijyobi.ac.jp>
2025-06-23 10:42:41 +0900

    プレイヤーリストの作成

--- server/player_state.py
+++ server/player_state.py
@@ -1,1 +1,13 @@
 #各プレイヤーの状態（位置・鬼かどうかなど）の管理
+class PlayerState:
+    def __init__(self, player_id, position=(0, 0)):
+        self.player_id = player_id
+        self.position = position
+        self.is_it = False # 鬼かどうか
+    def to_dict(self):
+        return {
+            "id" : self.player_id,
+            "position" : self.position,
+            "is_it" : self.is_it
+        }
+        
