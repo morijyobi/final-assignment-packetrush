c87db81c0765107a5303f3cced74d46f09471f68
kyo <k.nozaki.sys24@morijyobi.ac.jp>
2025-06-25 12:01:14 +0900

    サーバーへの接続要求

--- client/game.py
+++ client/game.py
@@ -4,46 +4,76 @@ import socket
 import json
 from player import Player
 from utils import config
+
+
 pg.init()
 screen = pg.display.set_mode((800, 600))
+
 # 背景画像
 haikeimg = pg.image.load("client/assets/images/map.png")
 haikeimg = pg.transform.scale(haikeimg, (800, 600))
+
 class Game:
     def __init__(self):
-        # 障害物画像を種類ごとに辞書で読み込み
+        # ソケット通信の初期化
+        self.server_addr = (config.SERVER_HOST, config.SERVER_PORT)
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket.settimeout(5)
+
+        self.player_id = None
+
+        # サーバーに接続要求を送信
+        self.send_connect_request()
+
+        # 障害物画像
         self.obstacle_images = {
-            "momiji" : pg.transform.scale(pg.image.load("client/assets/images/momiji.png"), (60, 60)), 
-            "ido" : pg.transform.scale(pg.image.load("client/assets/images/ido_gray.png"), (30, 30)),
-            "iwa" : pg.transform.scale(pg.image.load("client/assets/images/iwa_01.png"), (30, 30)),
-            "otera" : pg.transform.scale(pg.image.load("client/assets/images/otera.png"), (80, 80)),
-            "torii" : pg.transform.scale(pg.image.load("client/assets/images/torii01.png"), (80, 80))
-            }
-        # 障害物データ(種類と位置の組)
+            "momiji": pg.transform.scale(pg.image.load("client/assets/images/momiji.png"), (60, 60)),
+            "ido": pg.transform.scale(pg.image.load("client/assets/images/ido_gray.png"), (30, 30)),
+            "iwa": pg.transform.scale(pg.image.load("client/assets/images/iwa_01.png"), (30, 30)),
+            "otera": pg.transform.scale(pg.image.load("client/assets/images/otera.png"), (80, 80)),
+            "torii": pg.transform.scale(pg.image.load("client/assets/images/torii01.png"), (80, 80))
+        }
+
+        # 障害物データ
         self.obstacles = [
-            {"type" : "momiji", "pos" : (230, 180)},
-            {"type" : "momiji", "pos" : (230, 260)},
-            {"type" : "momiji", "pos" : (230, 340)},
-            {"type" : "otera", "pos" : (380, 50)},
-            {"type" : "torii", "pos" : (383, 130)},
-            {"type" : "iwa", "pos" : (350, 250)},
-            {"type" : "iwa", "pos" : (450, 330)},
-            {"type" : "ido", "pos" : (500, 90)},
-            {"type" : "momiji", "pos" : (530, 180)},
-            {"type" : "momiji", "pos" : (530, 260)},
-            {"type" : "momiji", "pos" : (530, 340)}
+            {"type": "momiji", "pos": (230, 180)},
+            {"type": "momiji", "pos": (230, 260)},
+            {"type": "momiji", "pos": (230, 340)},
+            {"type": "otera", "pos": (380, 50)},
+            {"type": "torii", "pos": (383, 130)},
+            {"type": "iwa", "pos": (350, 250)},
+            {"type": "iwa", "pos": (450, 330)},
+            {"type": "ido", "pos": (500, 90)},
+            {"type": "momiji", "pos": (530, 180)},
+            {"type": "momiji", "pos": (530, 260)},
+            {"type": "momiji", "pos": (530, 340)}
         ]
-        # 将来、ここでプレイヤーなども初期化
+
+    def send_connect_request(self):
+        connect_msg = {
+            "type": "connect_request",
+            "name": "Player1"
+        }
+        self.socket.sendto(json.dumps(connect_msg).encode(), self.server_addr)
+        try:
+            data, _ = self.socket.recvfrom(1024)
+            response = json.loads(data.decode())
+            if response.get("type") == "connect_ack":
+                self.player_id = response["player_id"]
+                print(f"[接続成功] プレイヤーID: {self.player_id}")
+            else:
+                print("[警告] サーバーから未知の応答:", response)
+        except socket.timeout:
+            print("[接続失敗] サーバーから応答なし")
+
     def draw(self):
-        # 背景を描画
         screen.blit(haikeimg, (0, 0))
-        # 障害物の描画
         for obs in self.obstacles:
             img = self.obstacle_images.get(obs["type"])
             if img:
                 screen.blit(img, obs["pos"])
         pg.display.flip()
-        
+
     def run(self):
         while True:
             self.draw()
@@ -51,7 +81,7 @@ class Game:
                 if event.type == pg.QUIT:
                     pg.quit()
                     sys.exit()
-        
+
 if __name__ == "__main__":
     game = Game()
     game.run()
