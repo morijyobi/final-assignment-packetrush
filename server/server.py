d4e8da38b99f86b528b807d9711c2fd1079c6188
kowada <t.kowada.sys24@morijyobi.ac.jp>
2025-06-25 12:28:42 +0900

    ユーザーの入力を元に座標を更新する処理

--- server/server.py
+++ server/server.py
@@ -7,6 +7,8 @@ import json
 HOST = '0.0.0.0'   # すべてのIPからの接続を受け付ける
 PORT = 12345       # 任意のUDPポート
 
+clients = []
+positions = {}
 # UDPソケット作成
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
@@ -15,10 +17,32 @@ server_socket.bind((HOST, PORT))
 print(f"🟢 サーバー起動: {HOST}:{PORT} で待機中...")
 
 # クライアントからのデータを受信して表示
-while True:
-    data, addr = server_socket.recvfrom(1024)  # 最大1024バイトまで受信
-    print(f"[受信] {addr} から: {data.decode()}")
-
-    # 確認用の応答を返す（任意）
-    reply = "受け取りました"
-    server_socket.sendto(reply.encode(), addr)
+def handle_client(conn, addr):
+    player_id = addr[len(clients)]
+    positions[player_id] = {"x": 0, "y": 0} #後で鬼ごっこ開始時の初期座標に更新しないといけない。
+    try:
+        while True:
+            data, addr = server_socket.recvfrom(1024)  # 最大1024バイトまで受信
+            reply = "受け取りました"
+            server_socket.sendto(reply.encode(), addr)
+            conn, addr = server_socket.accept()
+            clients.append(conn)
+            thread = threading.Thread(target=handle_client, args=(conn, addr), daemon=True)
+            thread.start()
+            if not data:
+                break
+            pos = json.loads(data.decode())
+            positions[player_id] = pos
+            broadcast(json.dumps(positions).encode())
+    except Exception as e:
+        print(f"エラー:{e}")
+    finally:
+        conn.close()
+        clients.remove(conn)
+        del positions[player_id]
+def broadcast(message):
+    for client in clients:
+        try:
+            client.sendall(message)
+        except:
+            pass 
