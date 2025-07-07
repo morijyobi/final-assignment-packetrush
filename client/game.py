f894ec36078cd93e77f5ff9ba7b9ea61ce47cee4
kowada <t.kowada.sys24@morijyobi.ac.jp>
2025-07-07 09:38:00 +0900

    ゲーム状態受信スレッドの作成

--- client/game.py
+++ client/game.py
@@ -10,24 +10,20 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from server.game_state import GameState
-
 pg.init()
 screen = pg.display.set_mode((config.SCREEN_WIDTH, config.SCREEN_HEIGHT))
-WHITE = (255, 255, 255)
+
 # 背景画像
 haikeimg = pg.image.load("client/assets/images/map.png")
 haikeimg = pg.transform.scale(haikeimg, (config.SCREEN_WIDTH, config.SCREEN_HEIGHT))
 
-# 制限時間
-total_time = 90
-start_time = pg.time.get_ticks()
 class Game:
     def __init__(self):
         pg.font.init()
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.socket.settimeout(5)
 
-        self.server_ip = ""
+        self.server_ip = "127.0.0.1"#後でタイトルかロビー画面で入力できるようにする
         self.server_port = config.SERVER_PORT
         self.server_addr = None
         self.player_id = None
@@ -80,7 +76,7 @@ class Game:
     # ロビーでのIP入力ループ
     def lobby_loop(self):
         while not self.ip_entered:
-            self.draw_lobby()
+            self.draw_lobby()#後でタイトル画面に変更する
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     pg.quit()
@@ -137,12 +133,30 @@ class Game:
                 screen.blit(img, obs["pos"])
         pg.display.flip()
 
+    def draw_title(self):
+        # タイトル画面(仮)
+        self.state = "title"  # タイトル状態に設定
+        screen.fill((60, 20, 20))
+        # print("ゲーム状態：タイトル")
+        font = pg.font.SysFont(None, 40)
+        text = font.render("ONI LINK", True, (255,255,255))
+        screen.blit(text,(100,250))
+        pg.display.flip()
     def draw_lobby(self):
         # ロビー画面の描画（仮）
         screen.fill((20, 20, 60))
+        # print("ゲーム状態：ロビー")
         font = pg.font.SysFont(None, 40)
         text = font.render("ロビー：ゲーム開始を待っています...", True, (255, 255, 255))
         screen.blit(text, (100, 250))
+        pg.display.flip()
+    def draw_result(self):
+        self.state = "result"  # 結果状態に設定
+        screen.fill((20,60,20))
+        # print("ゲーム状態：試合結果")
+        font = pg.font.SysFont(None,40)
+        text = font.render("OOteam Victory!", True,(255,255,255))
+        screen.blit(text,(100,250))
         pg.display.flip()
 
     def run(self):
@@ -156,13 +170,6 @@ class Game:
                 if event.type == pg.QUIT:
                     pg.quit()
                     sys.exit()
-
-            current_time = pg.time.get_ticks()
-            elapsed_time = (current_time - start_time) / 1000
-            remaining_time = total_time - elapsed_time
-            display_time = int(remaining_time)
-            timer_text = pg.font.Font(None, 74).render(f"Time: {display_time}", True, WHITE)
-            # キー入力チェック(キー押しっぱなし検出)
             keys = pg.key.get_pressed()
 
 # メイン処理
@@ -198,9 +205,6 @@ class Game:
             if Player.onirect.colliderect(Player.chararect1):
                 Player.chararect1.width = 0
                 Player.chararect1.height = 0
-                
-            text_rect = timer_text.get_rect(center=(800 // 2, 50))
-            screen.blit(timer_text, text_rect)
 
 
 if __name__ == "__main__":
